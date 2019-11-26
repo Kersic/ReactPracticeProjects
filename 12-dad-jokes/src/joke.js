29fb85eb06e3af98d83498c2424c81bfc5442307
TadejaK <tadeja.kersic@gmail.com>
2019-11-26 20:03:27 +0000

    dad jokes part 2

--- 12-dad-jokes/src/joke.js
+++ 12-dad-jokes/src/joke.js
@@ -2,16 +2,61 @@ import React from "react";
 import "./joke.css";
 
 function Joke({ joke, changeVote }) {
+  const getColor = () => {
+    if (joke.votes >= 15) {
+      return "#4CAF50";
+    } else if (joke.votes >= 12) {
+      return "#8BC34A";
+    } else if (joke.votes >= 9) {
+      return "#CDDC39";
+    } else if (joke.votes >= 6) {
+      return "#FFEB3B";
+    } else if (joke.votes >= 3) {
+      return "#FFC107";
+    } else if (joke.votes >= 0) {
+      return "#FF9800";
+    } else {
+      return "#f44336";
+    }
+  };
+
+  const getEmoji = () => {
+    if (joke.votes >= 15) {
+      return "em em-rolling_on_the_floor_laughing";
+    } else if (joke.votes >= 12) {
+      return "em em-laughing";
+    } else if (joke.votes >= 9) {
+      return "em em-smiley";
+    } else if (joke.votes >= 6) {
+      return "em em-slightly_smiling_face";
+    } else if (joke.votes >= 3) {
+      return "em em-neutral_face";
+    } else if (joke.votes >= 0) {
+      return "em em-confused";
+    } else {
+      return "em em-angry";
+    }
+  };
+
   return (
     <div className="Joke">
-      <div>
-        <button onClick={() => changeVote(joke.id, joke.votes + 1)}>up</button>
-        <p>{joke.votes}</p>
-        <button onClick={() => changeVote(joke.id, joke.votes - 1)}>
-          down
-        </button>
+      <div className="JokeButtons">
+        <i
+          className="fas fa-arrow-up"
+          onClick={() => changeVote(joke.id, joke.votes + 1)}
+        />
+        <span className="JokeVotes" style={{ borderColor: getColor() }}>
+          {joke.votes}
+        </span>
+        <i
+          className="fas fa-arrow-down"
+          onClick={() => changeVote(joke.id, joke.votes - 1)}
+        />
       </div>
-      <p>{joke.text}</p>
+      <div className="JokeText">{joke.text}</div>
+      <div className="JokeSmiley">
+        <i className={getEmoji()} />
+      </div>
     </div>
   );
 }
--- 12-dad-jokes/src/jokes.js
+++ 12-dad-jokes/src/jokes.js
@@ -1,36 +1,59 @@
 import React, { useState, useEffect } from "react";
 import Joke from "./joke";
+import "./Jokes.css";
 
 Jokes.defaultProps = {
-  numberOfJokes: 3
+  numberOfJokes: 10
 };
 
 function Jokes({ numberOfJokes }) {
   const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    addMoreJokes();
+    let currentJokes = JSON.parse(window.localStorage.getItem("jokes")) || [];
+    if (currentJokes.length === 0) addMoreJokes();
+    else setJokes(currentJokes);
   }, []);
 
+  useEffect(() => {
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+    setIsLoading(false);
+  }, [jokes]);
+
   const addMoreJokes = async () => {
-    let newJokes = [];
-    while (newJokes.length < numberOfJokes) {
-      const response = await fetch("https://icanhazdadjoke.com/", {
-        method: "GET",
-        headers: {
-          Accept: "application/json"
-        }
-      });
-      let data = await response.json();
-      const newJoke = {
-        votes: 0,
-        text: data.joke,
-        id: data.id
-      };
-      console.log(newJoke);
-      newJokes.push(newJoke);
+    try {
+      setIsLoading(true);
+      let newJokes = [];
+      while (newJokes.length < numberOfJokes) {
+        const response = await fetch("https://icanhazdadjoke.com/", {
+          method: "GET",
+          headers: {
+            Accept: "application/json"
+          }
+        });
+        let data = await response.json();
+        const newJoke = {
+          votes: 0,
+          text: data.joke,
+          id: data.id
+        };
+        if (
+          [...jokes, ...newJokes].filter(joke => joke.id === newJoke.id)
+            .length === 0
+        )
+          newJokes.push(newJoke);
+      }
+      setJokes([...jokes, ...newJokes]);
+
+      window.localStorage.setItem(
+        "jokes",
+        JSON.stringify([...jokes, ...newJokes])
+      );
+    } catch (e) {
+      alert(e);
+      setIsLoading(false);
     }
-    setJokes([...jokes, ...newJokes]);
   };
 
   const changeVote = (id, newVote) => {
@@ -42,20 +65,34 @@ function Jokes({ numberOfJokes }) {
     );
   };
 
+  const sortedJokes = jokes.sort((a, b) => b.votes - a.votes);
+
+  if (isLoading) {
+    return (
+      <div className="JokeListSpinner">
+        <i className="far fa-8x fa-laugh fa-spin" />
+        <h1 className="JokeListTitle">Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className="Jokes">
-      {/* <button onClick={addMoreJokes}>AddMore</button> */}
       <div className="JokesSidebar">
         <h1 className="JokeListTitle">
           <span>Dad</span> Jokes
         </h1>
-        <img>
-        </img>
+        <img
+          className="SmileImage"
+          src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+        />
+        <button className="GetMoreButton" onClick={addMoreJokes}>
+          Fetch Jokes
+        </button>
       </div>
       <div className="JokeList">
-        {jokes.map(joke => (
-          <p>{joke.text}</p>
-          // <Joke key={joke.id} joke={joke} changeVote={changeVote} />
+        {sortedJokes.map(joke => (
+          <Joke key={joke.id} joke={joke} changeVote={changeVote} />
         ))}
       </div>
     </div>
