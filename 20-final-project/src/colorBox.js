2831617e2bf9e6d931d73f6968b0dad510aec9f5
TadejaK <tadeja.kersic@gmail.com>
2019-11-30 15:49:19 +0000

    add copy overlay

--- 20-final-project/src/colorBox.js
+++ 20-final-project/src/colorBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createUseStyles } from "react-jss";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
@@ -61,14 +61,84 @@ const useStyles = createUseStyles({
     height: "30px",
     textAlign: "center",
     lineHeight: "30px"
+  },
+  copyOverlay: {
+    opacity: "0",
+    zIndex: "0",
+    width: "100%",
+    height: "100%",
+    transition: "transform 0.6s ease-in-out"
+  },
+  copyOverlayShow: {
+    opacity: "1",
+    transform: "scale(50)",
+    zIndex: "5",
+    position: "absolute"
+  },
+  copyMsg: {
+    position: "fixed",
+    left: "0",
+    right: "0",
+    top: "0",
+    bottom: "0",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "2rem",
+    transform: "scale(0.1)",
+    opacity: "0",
+    color: "white",
+    "& h1": {
+      fontWeight: "400",
+      background: "rgba(255, 255, 255, 0.2)",
+      width: "100%",
+      textAlign: "center",
+      marginBottom: "0",
+      padding: "1rem",
+      textTransform: "uppercase"
+      // [sizes.down("xs")]: {
+      //   fontSize: "6rem"
+      // }
+    },
+    "& p": {
+      fontSize: "2rem",
+      fontWeight: "100"
+    }
+  },
+  copyMsgShow: {
+    opacity: "1",
+    transform: "scale(1)",
+    zIndex: "25",
+    transition: "all 0.4s ease-in-out",
+    transitionDelay: "0.3s"
   }
 });
 
 function ColorBox({ color }) {
   const classes = useStyles();
+  const [showOverlay, setShowOverlay] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setShowOverlay(false), 1500);
+  }, [showOverlay]);
+
+  const changeOverlayState = () => {
+    setShowOverlay(true);
+  };
   return (
-    <CopyToClipboard text={color.color}>
+    <CopyToClipboard text={color.color} onCopy={changeOverlayState}>
       <div className={classes.colorBox} style={{ background: color.color }}>
+        <div
+          className={`${classes.copyOverlay} ${showOverlay &&
+            classes.copyOverlayShow}`}
+          style={{ background: color.color }}
+        />
+        <div
+          className={`${classes.copyMsg} ${showOverlay && classes.copyMsgShow}`}
+        >
+          <h1>Copied!</h1>
+          <p>{color.color}</p>
+        </div>
         <div className="copyContainer">
           <div className={classes.boxContent}>
             <span>{color.name}</span>
